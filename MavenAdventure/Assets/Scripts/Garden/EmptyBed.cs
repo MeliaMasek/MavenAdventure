f02edd529e31ba89ee8b057c8cd096bbc6290d15
Melia Masek <10910291@uvu.edu>
2025-02-11 22:10:59 -0700

    Updated seed inventory system

---
+++ MavenAdventure/Assets/Scripts/Garden/EmptyBed.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class EmptyBed : MonoBehaviour
+{
+    public bool isOccupied = false;
+}
---
+++ MavenAdventure/Assets/Scripts/Garden/PlantingSystem.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class PlantingSystem : MonoBehaviour
+{
+    public BackpackManager backpackManager;
+    public PlantManager plantManager;
+    public LayerMask bedLayer;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f, bedLayer))
+            {
+                EmptyBed bed = hit.collider.GetComponent<EmptyBed>();
+
+                if (bed != null && !bed.isOccupied)
+                {
+                    TryPlantSeed(bed);
+                }
+            }
+        }
+    }
+
+    private void TryPlantSeed(EmptyBed bed)
+    {
+        if (backpackManager.selectedSeed != null)
+        {
+            GameObject seedling = Instantiate(backpackManager.selectedSeed.seedPrefab, bed.transform.position, Quaternion.identity);
+            bed.isOccupied = true;
+
+            // Add the plant to PlantManager
+            PlantManager.Plant newPlant = new PlantManager.Plant
+            {
+                plantData = backpackManager.selectedSeed,
+                currentStage = 0,
+                currentStageObject = seedling,
+                isWatered = false
+            };
+
+            plantManager.plants.Add(newPlant);
+            Debug.Log($"Planted {backpackManager.selectedSeed.displayName}");
+
+            // Remove one seed from the backpack
+            backpackManager.RemoveItem(backpackManager.selectedSeed);
+        }
+        else
+        {
+            Debug.Log("No seed selected!");
+        }
+    }
+}
