a802e9d8d79b4ceb2ba32002c07def59d906c48f
Melia Masek <10910291@uvu.edu>
2025-04-15 16:53:17 -0600

    Final game updates + file reduction

--- MavenAdventure/Assets/Scripts/DataSystem/ReadOnlyAttr.cs
+++
@@ -1,4 +0,0 @@
-using UnityEngine;
-
-//Code borrowed and Modified by Dan Pos off of the inventory system series from youtube https://www.youtube.com/playlist?list=PL-hj540P5Q1hLK7NS5fTSNYoNJpPWSL24
-public class ReadOnlyAttr : PropertyAttribute{ }
